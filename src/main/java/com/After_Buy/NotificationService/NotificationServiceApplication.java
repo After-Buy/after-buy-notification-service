1fcd38a624eb9ab5472fce0f26bb18d5801c9066
tastysoapwater <gnslwkd13@gachon.ac.kr>
2026-04-11 22:39:14 +0900

    feat(notification): @EnableScheduling 추가
    WarrantyAlertScheduler의 @Scheduled 크론잡 동작을 위한 필수 어노테이션 추가
    누락 시 매일 09:00 보증 만료 알림 스케줄러가 실행되지 않음

--- src/main/java/com/After_Buy/NotificationService/NotificationServiceApplication.java
+++ src/main/java/com/After_Buy/NotificationService/NotificationServiceApplication.java
@@ -2,7 +2,9 @@ package com.After_Buy.NotificationService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class NotificationServiceApplication {
 
